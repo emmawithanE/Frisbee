db3f7b912b836b98e157cec215fdc98e259e2be3
emmawithanE <emma@catgirl.software>
2022-06-24 22:19:01 +0930

    Add greyscale and green/purple autotiles

--- scenes/Floor.gd
+++ scenes/Floor.gd
@@ -11,6 +11,9 @@ func _ready():
 	pass # Replace with function body.
 
 func ball_collision(ball, collision):
-	set_cellv(collision.get_collider().world_to_map(collision.get_position() - collision.get_normal()), 0)
 	ball.vel = ball.vel.bounce(collision.get_normal())
 	ball.increase_speed()
+
+	var cell = collision.get_collider().world_to_map(collision.get_position() - collision.get_normal())
+	var index = get_cell_autotile_coord(cell.x, cell.y)
+	set_cellv(cell, 1, false, false, false, index)
