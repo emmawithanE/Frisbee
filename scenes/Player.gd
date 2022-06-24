bf8fe59587e5fe992bd753a7c0701f65fb6c0f1a
emmawithanE <emma@catgirl.software>
2022-06-24 21:40:35 +0930

    Add prototype dash, awaiting movement rework

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,9 +4,11 @@ var colour = 2
 
 var vel = Vector2()
 const MAX_SPD = 100
-const GRAV = 20
-const JUMP = 600
+const GRAV = 10
+const JUMP = 300
+const JUMP_PEAK = 45
 const UP = Vector2(0,-1)
+
 
 enum ShootingStates {
 	Empty, Ready, Grabbing, GrabBackswing
@@ -17,6 +19,16 @@ const GRAB_BACKSWING_LENGTH = 0.5
 # Get bullet resource from outside
 export (PackedScene) var bullet
 
+enum DashState {
+	Ready, ChargingDash, Dash, NoDash
+}
+var dash_state = DashState.Ready
+var dash_charge = 0.0
+const DASH_SCALE = 0.5 # Dash length (s) = charge time * scale
+const MAX_DASH = 1 # Max dash length (s)
+const DASH_BACKSWING_LENGTH = 0.5
+const CHARGING_FALL_RATE = 0.5
+const DASH_SPEED = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,7 +36,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
+func _physics_process(delta):
 	$Pointing.look_at(get_global_mouse_position())
 	
 	var pointdir = fposmod($Pointing.rotation_degrees, 360)
@@ -46,7 +58,7 @@ func _physics_process(_delta):
 				# print("Shot fired.")
 				shooting_state = ShootingStates.Empty
 			ShootingStates.Empty:
-				$Timer.start(GRAB_LENGTH)
+				$GrabTimer.start(GRAB_LENGTH)
 				shooting_state = ShootingStates.Grabbing
 				print("setting ss to grabbing")
 			ShootingStates.Grabbing:
@@ -55,35 +67,53 @@ func _physics_process(_delta):
 				pass
 			_:
 				assert(false, "unhandled shooting state " + str(shooting_state))
-
-	vel.y += GRAV
 	
-#	if Input.is_action_pressed("ui_right"):
-#		vel.x = MAX_SPD
-#	elif Input.is_action_pressed("ui_left"):
-#		vel.x = -MAX_SPD
-#	else:
-#		vel.x = 0
-		
+	# Side to side movement	
 	vel.x = 0
-	
 	if Input.is_action_pressed("ui_right"):
 		vel.x += MAX_SPD
 	if Input.is_action_pressed("ui_left"):
 		vel.x -= MAX_SPD
 	
+	# Here there be jumping
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			vel.y = -JUMP
 	
+	# Handle dashing
+	if (Input.is_action_just_pressed("dash") && dash_state == DashState.Ready):
+		dash_state = DashState.ChargingDash
+	
+	if (dash_state == DashState.ChargingDash):
+		dash_charge += delta
+	
+	if (Input.is_action_just_released("dash") && dash_state == DashState.ChargingDash):
+		dash_state = DashState.Dash
+		$DashTimer.start(min(MAX_DASH, dash_charge * DASH_SCALE))
+		vel = get_local_mouse_position().normalized() * DASH_SPEED
+	# Gravity time
+	var gravity = 0
+	if (vel.y <= JUMP_PEAK && vel.y >= -JUMP_PEAK):
+		gravity += GRAV/2
+	else:
+		gravity += GRAV
+		
+	match dash_state:
+		DashState.ChargingDash:
+			gravity = min(gravity, CHARGING_FALL_RATE)
+		DashState.Dash:
+			gravity = 0
+	
+	vel.y += gravity
+
 	vel = move_and_slide(vel,UP)
 
-func timeout():
+func grab_timeout():
 	print("timeout")
 	match shooting_state:
 		ShootingStates.Grabbing:
 			shooting_state = ShootingStates.GrabBackswing
-			$Timer.start(GRAB_BACKSWING_LENGTH)
+			$GrabTimer.start(GRAB_BACKSWING_LENGTH)
 			print("setting ss to backswing")
 		ShootingStates.GrabBackswing:
 			print("setting ss to empty")
@@ -91,10 +121,22 @@ func timeout():
 		_:
 			assert(false, "shooting state=" + str(shooting_state) + " when timer ended")
 
+func dash_timeout():
+	match dash_state:
+		DashState.Dash:
+			dash_state = DashState.NoDash
+			$DashTimer.start(DASH_BACKSWING_LENGTH)
+			print("setting dash to backswing")
+		DashState.NoDash:
+			print("setting dash to ready")
+			dash_state = DashState.Ready
+		_:
+			assert(false, "dash state=" + str(dash_state) + " when timer ended")
+
 func ball_collision(ball, collision):
 	print("ball collision")
 	print("ss == " + str(shooting_state))
 	if shooting_state == ShootingStates.Grabbing:
 		ball.die()
 		shooting_state = ShootingStates.Ready
-		$Timer.stop()
+		$GrabTimer.stop()
