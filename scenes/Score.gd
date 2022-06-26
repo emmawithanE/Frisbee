a0e7a1cc08dd9f132c3fcd63e6117291e777490f
emmawithanE <emma@catgirl.software>
2022-06-26 12:50:15 +0930

    Draw lives

--- scenes/Score.gd
+++ scenes/Score.gd
@@ -11,6 +11,11 @@ var running = true
 
 const DISPLAY_SCALE = 0.01
 
+const HEADS_ELEVATION = 32
+const HEADS_SPACING = 32
+var green_head = load("res://sprites/greenhead.png")
+var purple_head = load("res://sprites/purplehead.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Signals.connect("paint_area_changed", self, "on_paint")
@@ -36,6 +41,7 @@ func on_tick():
 
 	$GreenScore.set_text(str(int(green * DISPLAY_SCALE)))
 	$PurpleScore.set_text(str(int(purple * DISPLAY_SCALE)))
+	update()
 
 func on_kill(player):
 	if player.colour == 1:
@@ -56,3 +62,16 @@ func lives_changed(green, purple):
 	print("green lives " + str(green_lives) + " purple " + str(purple_lives))
 	$GreenScore.set_text(str(int(green * DISPLAY_SCALE)))
 	$PurpleScore.set_text(str(int(purple * DISPLAY_SCALE)))
+	
+func _draw():
+	print("drawing")
+	# Draw green heads above green score
+	var green_pos = $GreenScore.get_position() - Vector2(0,HEADS_ELEVATION)
+	print("green_pos: " + str(green_pos))
+	for i in green_lives:
+		draw_texture(green_head, green_pos + Vector2(i * HEADS_SPACING, 0))
+		
+	# Draw purple heads above purple score
+	var purple_pos = $PurpleScore.get_position() - Vector2(0,HEADS_ELEVATION)
+	for i in purple_lives:
+		draw_texture(purple_head, purple_pos - Vector2(i * HEADS_SPACING, 0))
